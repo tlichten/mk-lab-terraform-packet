72467b85cd50a8a71e3aed84b95aaf7c6de36c62
tlichten <tlichtenstein@mirantis.com>
2016-12-01 20:45:19 +0100

    Pinning salt version 2016.3

--- templates/master.sh
+++ templates/master.sh
@@ -9,8 +9,8 @@ which wget >/dev/null || (apt-get update; apt-get install -y wget)
 echo "deb [arch=amd64] http://apt.tcpcloud.eu/nightly/ xenial main security extra tcp" > /etc/apt/sources.list
 curl http://apt.tcpcloud.eu/public.gpg | apt-key add -
 
-echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list
-curl https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/2016.3 xenial main" > /etc/apt/sources.list.d/saltstack.list
+curl https://repo.saltstack.com/apt/ubuntu/16.04/amd64/2016.3/SALTSTACK-GPG-KEY.pub | apt-key add -
 
 apt-get clean
 apt-get update
